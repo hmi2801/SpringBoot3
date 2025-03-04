fa7843d1123587edeba8d92603221de5c6d72af3
harsh.misra <harshmishra2801@gmail.com>
2025-03-04 11:09:01 +0530

    get request

---
+++ src/main/java/Controller/GreetingController.java
@@ -0,0 +1,34 @@
+package Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("greetings")
+public class GreetingController {
+
+    GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    //UC1
+    @GetMapping("/get")
+    public String getGreetings(){
+        return "{\"message\": \"Hello from GET Request!\"}";
+    }
+
+    @PostMapping("/post")
+    public String postGreetings(@RequestBody MessageDTO message){
+        return "{\""+message.getMessage()+": \"Hello from POST Request!\"}";
+    }
+
+    @PutMapping("/put/{message}")
+    public String putGreetings(@PathVariable String message){
+        return "{\""+message+": \"Hello from PUT Request!\"}";
+    }
+
+
+
+}
