7d231d778242d5e83314013f9de7d7cb0b13700c
harsh.misra <harshmishra2801@gmail.com>
2025-03-04 21:39:36 +0530

    registration

---
+++ src/main/java/com/example/SpringBoot3/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.SpringBoot3.controllers;
+
+
+import com.example.SpringBoot3.dto.LoginDTO;
+import com.example.SpringBoot3.dto.MailDTO;
+import com.example.SpringBoot3.dto.MessageDTO;
+import com.example.SpringBoot3.dto.AuthUserDTO;
+import com.example.SpringBoot3.services.AuthenticationService;
+import com.example.SpringBoot3.services.EmailService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    EmailService emailService;
+    AuthenticationService authenticationService;
+
+    public UserController(EmailService emailService, AuthenticationService authenticationService) {
+        this.emailService = emailService;
+        this.authenticationService = authenticationService;
+    }
+
+    //UC9 --> For Registration of a user
+    @PostMapping(path = "/register")
+    public String register(@RequestBody AuthUserDTO user) {
+        return authenticationService.register(user);
+    }
+
+}
